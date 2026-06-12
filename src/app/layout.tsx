96951f76a1f0e6f5fe262407ca8b27fa7fc6c281
Your Name <singhvikas3171997@gmail.com>
2026-06-13 03:52:02 +0530

    gad

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,23 @@
 import type { ReactNode } from "react"
+import Script from "next/script"
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html>
+      <head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=AW-18230881098"
+          strategy="afterInteractive"
+        />
+        <Script id="gtag-init" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'AW-18230881098');
+          `}
+        </Script>
+      </head>
       <body>{children}</body>
     </html>
   )
